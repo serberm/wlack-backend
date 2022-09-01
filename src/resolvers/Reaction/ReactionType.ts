c66e87152b08bbb3ecb2487caff027d594eace94
serberm <andriiivanytskyi@outlook.com>
2022-09-01 16:30:10 -0400

    Reaction type

--- src/resolvers/Reaction/ReactionType.ts
+++ src/resolvers/Reaction/ReactionType.ts
@@ -1,14 +1,14 @@
 import { objectType } from 'nexus'
+import { Message, User } from '../index'
 
 export const Reaction = objectType({
   name: 'Reaction',
   definition(t) {
-    t.model.id()
-    t.model.name()
-    t.model.createdAt()
-    t.model.updatedAt()
-    t.model.users({ pagination: false })
-    t.model.message()
+    t.nonNull.field('createdAt', { type: 'DateTime' })
+    t.nonNull.string('id')
+    t.nonNull.field('message', { type: Message })
+    t.nonNull.string('name')
+    t.nonNull.field('updatedAt', { type: 'DateTime' })
+    t.nonNull.list.nonNull.field('users', { type: User })
   }
-});
-
+})
