9ea6c60903bde0da2327c93e5164e455b15fc4df
serberm <andriiivanytskyi@outlook.com>
2023-01-02 20:46:32 -0500

    channel

--- src/resolvers/Channel/ChannelQuery.ts
+++ src/resolvers/Channel/ChannelQuery.ts
@@ -1,12 +1,11 @@
-import { queryField, stringArg, list } from 'nexus'
-import { getUserId, getTenant } from '../../utils'
+import { queryField, stringArg, list, nonNull } from 'nexus'
+import { getUserId } from '../../utils'
 
 export const channels = queryField('channels', {
   type: list('Channel'),
-
-  args: { communityUrl: stringArg() },
-  resolve: (parent, { communityUrl }, Context) => {
-    return Context.prisma.channel.findMany({
+  args: { communityUrl: nonNull(stringArg()) },
+  resolve: (parent, { communityUrl }, ctx) => {
+    return ctx.prisma.channel.findMany({
       where: {
         community: {
           url: communityUrl
@@ -18,13 +17,12 @@ export const channels = queryField('channels', {
 })
 
 export const channel = queryField('channel', {
-  type: list('Channel'),
-
+  type: 'Channel',
   args: {
-    url: stringArg()
+    url: nonNull(stringArg())
   },
-  resolve: (parent, { url }, Context) => {
-    return Context.prisma.channel.findFirst({
+  resolve: (parent, { url }, ctx) => {
+    return ctx.prisma.channel.findFirst({
       where: { url }
     })
   }
@@ -32,20 +30,21 @@ export const channel = queryField('channel', {
 
 export const privateChannels = queryField('privateChannels', {
   type: list('Channel'),
-
-  resolve: async (_parent, {}, Context) => {
-    const userId = await getUserId(Context)
-
-    const user = await Context.prisma.user.findFirst({
+  resolve: async (_parent, {}, ctx) => {
+    const userId = await getUserId(ctx)
+    if (!userId) {
+      throw new Error('nonexistent user')
+    }
+    const user = await ctx.prisma.user.findFirst({
       where: {
         id: userId
       }
     })
 
     const searchString = 'direct/'
-    const searchString1 = user.username + '-'
+    const searchString1 = user!.username + '-'
 
-    const channels = await Context.prisma.channel.findMany({
+    const channels = await ctx.prisma.channel.findMany({
       where: {
         AND: [{ url: { contains: searchString } }, { url: { contains: searchString1 } }]
       },
