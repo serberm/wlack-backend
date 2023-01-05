613434da2a2379965d87c0d7c39dcce0b60fc7f1
serberm <andriiivanytskyi@outlook.com>
2023-01-05 04:09:17 -0500

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,30 +1,31 @@
 import { verify } from 'jsonwebtoken'
-import { Context } from '../types'
+import { Context } from '../context'
 
 interface Token {
   userId: string
 }
 
-export function getUserId(context: Context) : string {
-  const Authorization = context.request.get('Authorization')
+export function getUserId(context: Context): string | undefined {
+  const Authorization = context.req.headers['authorization']
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '')
     const verifiedToken = verify(token, process.env['APP_SECRET']) as Token
     return verifiedToken && verifiedToken.userId
   }
-  return null;
+  return undefined
 }
 
-export function getTenant(context: Context) : string {
-  const tenantName = context.request.get('wlack-tenant')
-  if (tenantName) {
-    return tenantName;
-  }
-  return null;
+export function getTenant(context: Context): string {
+  // const tenantName = context.request.get('ciscord-tenant')
+  // if (tenantName) {
+  //   return tenantName;
+  // }
+  // return null;
+  return 'test'
 }
 
 export const isEmpty = (value: any): boolean =>
   value === undefined ||
   value === null ||
-  (typeof value === "object" && Object.keys(value).length === 0) ||
-  (typeof value === "string" && value.trim().length === 0);
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0)
